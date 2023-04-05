811687fd043ed593a7be6609e1a077350b5cf560
Abed <abedurrahman03@gmail.com>
2023-04-05 15:44:16 +0600

    Initial Commit

---
+++ movieTrending/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  movieTrending
+//
+//  Created by BS1095 on 5/4/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: MainViewController())
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+        
+        self.window = window
+        
+        return true
+    }
+
+}
+
---
+++ movieTrending/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  movieTrending
+//
+//  Created by BS1095 on 5/4/23.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Main View okay??"
+        self.view.backgroundColor = .green
+    }
+
+
+}
